6ac329fc5884b45cc138c7620bcfb64d9a3f6c96
chandancosmos <chandankumar.pandey@cognizant.com>
2025-10-15 10:03:03 +0530

    header made common

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ResultsComponent } from "./student/results/results.component";
+import { Header } from "./header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ResultsComponent],
+  imports: [RouterOutlet, ResultsComponent, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
