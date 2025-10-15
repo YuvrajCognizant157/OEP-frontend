eb7959987f8e294a4c8868da20dc3a76cc8d9827
chandancosmos <chandankumar.pandey@cognizant.com>
2025-10-15 10:09:13 +0530

    Footer shared

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ResultsComponent } from "./student/results/results.component";
 import { Header } from "./header/header";
+import { Footer } from "./footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ResultsComponent, Header],
+  imports: [RouterOutlet, ResultsComponent, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
