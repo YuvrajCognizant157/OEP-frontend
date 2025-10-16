6ad3610d6225b6732cb8ca77edb3e1d0813cdded
Indresh Maurya <2439831@cognizant.com>
2025-10-16 14:59:40 +0530

    register-student and register-employee

---
+++ src/app/register/register-employee/register.employee.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+import { RegisterEmployeeRequest } from './register-employee.model';
+import { RegisterEmployeeService } from '../../core/services/register-employee.service';
+
+@Component({
+  selector: 'app-register-employee',
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ],
+  templateUrl: './register-employee.html',
+  styleUrls: ['./register-employee.css']
+})
+export class RegisterEmployeeComponent {
+  email = '';
+  fullName = '';
+  password = '';
+  dob: string = '';
+  phoneNo = '';
+  role = 'Examiner';
+  token = '';
+  errorMessage = '';
+  successMessage = '';
+
+  constructor(private registerService: RegisterEmployeeService, private router: Router) {}
+
+  onSubmit(): void {
+    const registerData: RegisterEmployeeRequest = {
+      email: this.email,
+      fullName: this.fullName,
+      password: this.password,
+      dob: this.dob,
+      phoneNo: this.phoneNo,
+      role: this.role,
+      token: this.token
+    };
+
+    this.registerService.registerExaminer(registerData).subscribe({
+      next: () => {
+        this.successMessage = 'Registration successful!';
+        this.router.navigate(['/login']);
+      },
+      error: (err) => {
+        this.errorMessage = 'Registration failed. Please try again.';
+        console.error(err);
+      }
+    });
+  }
+}
