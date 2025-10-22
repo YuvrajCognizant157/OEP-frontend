e890e5466c01ceffc414e7663985edf174178023
Aditya Pathak <2439797@cognizant.com>
2025-10-22 21:48:27 +0530

    examiner navbar

--- src/app/examiner/navbar/navbar.ts
+++ src/app/examiner/navbar/navbar.ts
@@ -5,14 +5,31 @@ import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, MatToolbarModule,MatButtonModule],
+  imports: [RouterLink, MatToolbarModule,MatButtonModule,CommonModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class Navbar {
+
+  isMenuOpen = false;
+toggleMenu() {
+ this.isMenuOpen = !this.isMenuOpen;
+}
+closeMenu() {
+ this.isMenuOpen = false;
+}
+ngOnInit(): void {
+ // Auto-close drawer when resizing to desktop view
+ window.addEventListener('resize', () => {
+   if (window.innerWidth > 950 && this.isMenuOpen) {
+     this.isMenuOpen = false;
+   }
+ });
+}
   private breakpointObserver = inject(BreakpointObserver);
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
