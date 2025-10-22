62e05c24f13578c84b338106c526bf25ef98fddf
Aditya Pathak <2439797@cognizant.com>
2025-10-22 19:08:03 +0530

    new changes design

--- src/app/admin/approve-topic/approve-topic.ts
+++ src/app/admin/approve-topic/approve-topic.ts
@@ -2,12 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 import { AdminService } from '../../core/services/admin.service';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
 
   selector: 'app-approve-topic',
   standalone:true,
-  imports:[CommonModule],
+  imports:[CommonModule,MatCardModule,MatButtonModule,MatProgressSpinnerModule],
 
   templateUrl: './approve-topic.html',
 
