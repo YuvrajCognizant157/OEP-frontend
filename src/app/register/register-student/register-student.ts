24d754f1e7d57212709d36c6d66fa87e86718520
Yuvraj Chaturvedi <2440157@cognizant.com>
2025-10-22 22:07:18 +0530

    updated student-registration

--- src/app/register/register-student/register-student.ts
+++ src/app/register/register-student/register-student.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'; // Import spinner module
 
 import { RegisterStudentRequest } from './register-student.model';
 import { RegisterStudentService } from '../../core/services/register-student.service';
@@ -24,7 +25,8 @@ interface RegisterResponse {
     FormsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './register-student.html',
   styleUrls: ['./register-student.css']
@@ -37,10 +39,16 @@ export class RegisterStudentComponent {
   dob: string = '';
   errorMessage = '';
   successMessage = signal<string>("");
+  isLoading = false;
 
   constructor(private registerService: RegisterStudentService, private router: Router,private authService : AuthService) {}
 
   onSubmit(): void {
+
+   this.isLoading = true;
+    this.errorMessage = '';
+    this.successMessage.set('');
+
     const registerData: RegisterStudentRequest = {
       fullName: this.fullName,
       email: this.email,
@@ -51,23 +59,21 @@ export class RegisterStudentComponent {
 
     this.registerService.registerStudent(registerData).subscribe({
     next: (response:RegisterResponse) => {
+      this.isLoading = false; 
       this.successMessage.set(response.msg);
 
       const newUserId = response.userId; 
-      if (!newUserId) {
-        throw Error;
-      }
-      this.authService.pendingVerificationUserId.set(newUserId);
-
-      if(response.userId != null){
-        this.router.navigate([`/verify-otp/${newUserId}`]); 
-      }
-       else {
-        alert("Some Internal Server Error");
-        this.router.navigate(['/login']);
-      }
+      if (newUserId) { 
+          this.authService.pendingVerificationUserId.set(newUserId);
+          this.router.navigate([`/verify-otp/${newUserId}`]);
+        } else {
+          // Handle case where API succeeds but doesn't return a valid ID
+          this.errorMessage = "Registration successful, but an error occurred. Please try logging in.";
+          this.router.navigate(['/login']);
+        }
     },
     error: (err) => {
+      this.isLoading = false; 
       this.errorMessage = err.error?.error || 'Registration failed. Please try again.';
       console.error(err);
     }
