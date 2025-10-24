57672a1a6907da6974aee19578d4db28f2b011ab
chandancosmos <chandankumar.pandey@cognizant.com>
2025-10-24 23:17:39 +0530

    student-navbar

--- src/app/core/services/layout.service.ts
+++ src/app/core/services/layout.service.ts
@@ -9,9 +9,13 @@ export class LayoutService {
   // Default to true (visible)
   private layoutVisible = signal(true);
 
+  private sLayoutVisible = signal(true);
+
   // 2. Expose it as a public, readonly signal
   // Components can read this, but not set it.
   public isLayoutVisible = this.layoutVisible.asReadonly();
+
+  public isSLayoutVisible = this.sLayoutVisible.asReadonly();
 
   constructor() { }
 
@@ -19,7 +23,14 @@ export class LayoutService {
     this.layoutVisible.set(true); // Use .set() to update
   }
 
+  showSLayout() {
+    this.sLayoutVisible.set(true); // Use .set() to update
+  }
+
   hideLayout() {
     this.layoutVisible.set(false); // Use .set() to update
+  }
+  hideSLayout() {
+    this.sLayoutVisible.set(false); // Use .set() to update
   }
 }
--- src/app/student/routing/routing.ts
+++ src/app/student/routing/routing.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { SNavbar } from "../s-navbar/s-navbar";
+import { CommonModule } from '@angular/common';
+import { LayoutService } from '../../core/services/layout.service';
 
 @Component({
   selector: 'app-routing',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SNavbar, CommonModule],
   templateUrl: './routing.html',
   styleUrl: './routing.css'
 })
 export class Routing {
+
+   private sLayoutService = inject(LayoutService);
+  isSLayoutVisible = this.sLayoutService.isSLayoutVisible;
 
 }
