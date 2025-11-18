daf3d020a108095e183fe2ec138ccf5b53dbd717
chandancosmos <chandankumar.pandey@cognizant.com>
2025-11-18 14:53:34 +0530

    trying fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // This tells Angular to use the variable 'NG_APP_API_URL'
   // from the build environment (which you set on Netlify)
-  apiUrl: '' 
+  apiUrl: (((window as any).process && (window as any).process.env && (window as any).process.env.NG_APP_SERVER_API) as string) || ''
 };
