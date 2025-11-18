bbd22460fc84d3be766430d98c46cb148ede6632
Chandan <chandankumar.pandey@cognizant.com>
2025-11-18 14:10:09 +0530

    Update apiUrl to an empty string in production config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // This tells Angular to use the variable 'NG_APP_API_URL'
   // from the build environment (which you set on Netlify)
-  apiUrl: process.env['NG_APP_API_URL'] 
+  apiUrl: '' 
 };
